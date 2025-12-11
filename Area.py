b2ea8f40838070ca55ba32c1ed354942b2b7ef78
ShivtejPharane <pharaneshiva@gmail.com>
2025-12-11 10:05:13 +0530

    1 st

---
+++ Area.py
@@ -0,0 +1,8 @@
+def rectangle(l,b):
+    return l*b
+
+def square(l):
+    return l*l
+
+def cricle(r):
+    return 3.14*r*r
---
+++ main.py
@@ -0,0 +1,7 @@
+import Area as a
+l=10
+b=5
+r=7
+#print(a.rectangle(l,b))
+print(a.cricle(r))
+print(a.square(l))
